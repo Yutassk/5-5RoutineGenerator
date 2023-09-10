89f1dc55195b9c7fc153c62846e8cb1f111dd8b5
Yutassk <sasaki.yt01@gmail.com>
2023-09-10 19:52:57 +0900

    🎨 CSS調整

---
+++ src/app/component/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mb-4">
+      <h1 className="text-3xl bg-lime-400 text-center text-white my-2 p-2">Training Generator</h1>
+    </div>
+  );
+};
+
+export default Header;
